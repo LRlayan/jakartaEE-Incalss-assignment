91ceb2ad34ad8699117d421665f05c4b409b63f6
LRLayan <rameshlayan4@gmail.com>
2024-08-31 10:47:20 +0530

    commit

---
+++ src/main/java/com/example/controller/Spring.java
@@ -0,0 +1,10 @@
+package com.example.controller;
+
+@SpringBootApplication
+public class Spring {
+        public static void main(String[] args) {
+            Spring.run(Spring.class,args);
+        }
+
+    }
+}
---
+++ src/main/java/com/example/controller/config/config.java
@@ -0,0 +1,5 @@
+package com.example.controller.config;
+
+@Configuration
+public class config {
+}
