a0bfa1ca3ea259f5695587ed85739fe0117fbbc0
LRLayan <rameshlayan4@gmail.com>
2024-08-31 10:48:34 +0530

    commit

--- src/main/java/com/example/controller/Chickenbun.java
+++ src/main/java/com/example/controller/Chickenbun.java
@@ -1,4 +1,5 @@
 package com.example.controller;
 
+@Component
 public class Chickenbun {
 }
--- src/main/java/com/example/controller/Spring.java
+++ src/main/java/com/example/controller/Spring.java
@@ -5,6 +5,4 @@ public class Spring {
         public static void main(String[] args) {
             Spring.run(Spring.class,args);
         }
-
-    }
 }
